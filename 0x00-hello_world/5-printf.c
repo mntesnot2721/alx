2baf95cf0eebae662a96aa39634f8b03d7e7e287
mntesnot443 <mntesnotasmamaw443@gmail.com>
2023-02-16 04:56:40 -0800

    PRINTF

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - A program that print a line with printf
+ * main - A program that print a line with printf function
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
