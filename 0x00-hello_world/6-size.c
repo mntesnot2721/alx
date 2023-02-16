359b38228f01a3fabb8eaf07c8bf52c0ef0a2eaf
mntesnot443 <mntesnotasmamaw443@gmail.com>
2023-02-16 05:35:16 -0800

    SIZE

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("size of a char: %lu.\n", (unsigned long)sizeof(a));
-	printf("size of an int: %lu.\n", (unsigned long)sizeof(b));
-	printf("size of a long int: %lu.\n", (unsigned long)sizeof(c));
-	printf("size of a long long int: %lu.\n", (unsigned long)sizeof(d));
-	printf("size of a float: %lu.\n", (unsigned long)sizeof(f));
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+	printf("size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
