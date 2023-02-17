bea2e8b79dd13c6babd1bf03ba97a3c3baf3c2ea
mntesnot443 <mntesnotasmamaw443@gmail.com>
2023-02-17 04:29:20 -0800

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+#include <ctype.h>
 /**
- * main  print the hexadecimal numbers
- * code by Mntesnot
- * Return: 0
- */
+*main - Prints all single digits of base 16 followed by new line
+*
+*Return: returns 0
+*/
 int main(void)
 {
-	int letter
+	int digit;
 
 	for (digit = '0'; digit <= '9'; digit++)
 	{
-	putchar(digit);
-	if (digit == '9')
+		putchar(digit);
+		if (digit == '9')
 		{
 			digit = 'a';
 			for (; digit <= 'f'; digit++)
