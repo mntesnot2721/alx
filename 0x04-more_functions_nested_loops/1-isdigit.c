4b061ae5c9af087d89d94318d7ec52e31ab8c0a6
mntesnot443 <mntesnotasmamaw443@gmail.com>
2023-02-23 01:01:23 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,15 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
  * _isdigit - Check for a digit (0-9)
  * @c: Integer holding the character
  *
  * Return: 1- Digit 0- Not
  */
+
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 		return (1);
+
 	return (0);
 }
